d5728df391f0575a1150f6d9474f5fb5644d3db3
jjhjhh <nongsh1m@naver.com>
2024-07-01 16:26:28 +0900

    Update app.py

--- app.py
+++ app.py
@@ -19,15 +19,12 @@ def website():
 @app.route("/login",methods=['GET','POST'])
 def login():
     print("login endpoint")
-    if request.method=="POST":  #아ㅜㅜ socket_io 추가하면 안 되던 이유가 request가 밑에 함수명이랑 겹쳐서 그런거였음 
+    if request.method=="POST": 
         id=request.form['id']
         pw=request.form['pw']
         
         if id in users and users[id]==pw:
             session['id']=id
-            return redirect(url_for('chatting'))
-        elif id=="admin'-- " or id=="admin'#" or id=="'or 1=1#": #DB를 안 썼더니 SQLinjection이 안 돼서 임의로 열어둠.. ㅁㅊ;;
-            session['id']='admin'
             return redirect(url_for('chatting'))
         else:
             return render_template('index.html', error='Invalid username or password')
